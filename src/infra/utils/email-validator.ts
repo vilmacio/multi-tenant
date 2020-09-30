97e6b0affe903b7d6d1b81c4465cae3781a4b94f
vilmacio <vilmacio@users.noreply.github.com>
2020-09-30 14:31:20 -0300

    feat: validator implementation

---
+++ src/infra/utils/email-validator.ts
@@ -0,0 +1,10 @@
+import { EmailValidator } from '../../presentation/protocols/email-validator'
+
+import validator from 'validator'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string):boolean {
+    const isValid = validator.isEmail(email)
+    return isValid
+  }
+}
