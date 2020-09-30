ebad66d50d4a3fe7aa6d3eb0d78de1a924fff702
vilmacio <vilmacio@users.noreply.github.com>
2020-09-30 17:09:41 -0300

    feat: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/login.ts
+++ src/presentation/controllers/login.ts
@@ -1,19 +1,22 @@
 import { InvalidParamError } from '../errors/invalid-param'
 import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest, serverError } from '../helpers/http-helper'
+import { badRequest, ok, serverError } from '../helpers/http-helper'
+import { Authentication } from '../protocols/authentication'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { httpRequest, httpResponse } from '../protocols/http'
 
 export class LoginController implements Controller {
     private emailValidator: EmailValidator
-    constructor (emailvalidator:EmailValidator) {
+    private authentication: Authentication
+    constructor (emailvalidator:EmailValidator, authentication: Authentication) {
       this.emailValidator = emailvalidator
+      this.authentication = authentication
     }
 
     async handle (httpRequest: httpRequest):Promise<httpResponse> {
       try {
-        const { email } = httpRequest.body
+        const { email, password } = httpRequest.body
         const requiredFields = ['email', 'password']
         for (const field of requiredFields) {
           if (!httpRequest.body[field]) {
@@ -25,6 +28,9 @@ export class LoginController implements Controller {
         if (!isValid) {
           return badRequest(new InvalidParamError('email'))
         }
+
+        const token = await this.authentication.auth(email, password)
+        return ok({ access_token: token })
       } catch (error) {
         return serverError(error)
       }
