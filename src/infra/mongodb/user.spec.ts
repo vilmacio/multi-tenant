b073f3a563887ca42df2cb2e889df120e35c7f06
vilmacio <vilmacio@users.noreply.github.com>
2020-10-04 15:07:11 -0300

    test: ensure UserMongoAdapter returns a user on success

---
+++ src/infra/mongodb/user.spec.ts
@@ -0,0 +1,39 @@
+import { AddUserModel } from '../../domain/usecases/add-user'
+import { MongoHelper } from './helper'
+import { UserMongoRepository } from './user'
+
+const makeSut = () => {
+  return new UserMongoRepository()
+}
+
+describe('User', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URI)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.desconnect()
+  })
+
+  beforeEach(async () => {
+    const userCollection = MongoHelper.getCollection('users')
+    await userCollection.deleteMany({})
+  })
+
+  describe('save()', () => {
+    test('Should return a user on success', async () => {
+      const sut = makeSut()
+      const addUserModel:AddUserModel = {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password'
+      }
+      const user = await sut.save(addUserModel)
+      expect(user).toBeTruthy()
+      expect(user.id).toBeTruthy()
+      expect(user.name).toBe(addUserModel.name)
+      expect(user.email).toBe(addUserModel.email)
+      expect(user.password).toBe(addUserModel.password)
+    })
+  })
+})
