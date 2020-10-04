a2f00af6e34c26443d586c48ba1584c0a4369bed
vilmacio <vilmacio@users.noreply.github.com>
2020-10-04 15:37:33 -0300

    feat: ensure compare method into BCryptAdapter returns a boolean

--- src/data/login/protocols/hash-comparer.ts
+++ src/data/login/protocols/hash-comparer.ts
@@ -1,3 +1,3 @@
 export interface HashComparer {
-    compare(hash:string, valueToCompare:string):Promise<boolean>
+    compare(value:string, hash:string):Promise<boolean>
 }
--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,14 +1,20 @@
 import { Hasher } from '../../data/signup/protocols/hasher'
 import bcrypt from 'bcrypt'
+import { HashComparer } from '../../data/login/protocols/hash-comparer'
 
-export class BCryptAdapter implements Hasher {
+export class BCryptAdapter implements Hasher, HashComparer {
     private salt: number
     constructor (salt:number) {
       this.salt = salt
     }
 
-    async hash (value: string) {
+    async hash (value:string) {
       const hashedValue = await bcrypt.hash(value, this.salt)
       return hashedValue
+    }
+
+    async compare (hash:string, valueToCompare:string):Promise<boolean> {
+      const result = await bcrypt.compare(hash, valueToCompare)
+      return result
     }
 }
