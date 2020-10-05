5b2ff5edec10b8dd9cb5b2bc5fa58c7385f75cce
vilmacio <vilmacio@users.noreply.github.com>
2020-10-05 09:41:03 -0300

    feat: ensure loadByEmail return a user

--- src/infra/mongodb/user.ts
+++ src/infra/mongodb/user.ts
@@ -1,13 +1,27 @@
+import { Collection } from 'mongodb'
+import { LoadByEmailRepository } from '../../data/login/protocols/load-by-email-repository'
 import { SaveUserRepository } from '../../data/signup/protocols/save-user-repository'
 import { User } from '../../domain/models/user'
 import { AddUserModel } from '../../domain/usecases/add-user'
 import { MongoHelper } from './helper'
 
-export class UserMongoRepository implements SaveUserRepository {
+export class UserMongoRepository implements SaveUserRepository, LoadByEmailRepository {
+  private userCollection:Collection
+
+  constructor () {
+    this.userCollection = MongoHelper.getCollection('users')
+  }
+
   async save (addUserModel:AddUserModel):Promise<User> {
-    const userCollection = MongoHelper.getCollection('users')
-    const result = await userCollection.insertOne(addUserModel)
+    const result = await this.userCollection.insertOne(addUserModel)
     const { _id, ...account } = result.ops[0]
+    account.id = _id
+    return account
+  }
+
+  async loadByEmail (email:string):Promise<User> {
+    const result = await this.userCollection.findOne({ email })
+    const { _id, ...account } = result
     account.id = _id
     return account
   }
