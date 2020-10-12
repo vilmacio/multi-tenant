57ff07f190eec16c54da03d1fb5a95e9e56ae3da
vilmacio <vilmacio@users.noreply.github.com>
2020-10-12 15:01:02 -0300

    feat: ensures loadById implementation returns a user on seccess

--- src/infra/mongodb/user.ts
+++ src/infra/mongodb/user.ts
@@ -1,10 +1,11 @@
+import { LoadByIdRepository } from '../../data/authorization/protocols/load-by-id-repository'
 import { LoadByEmailRepository } from '../../data/login/protocols/load-by-email-repository'
 import { SaveUserRepository } from '../../data/signup/protocols/save-user-repository'
 import { User } from '../../domain/models/user'
 import { AddUserModel } from '../../domain/usecases/add-user'
 import { MongoHelper } from './helper'
 
-export class UserMongoRepository implements SaveUserRepository, LoadByEmailRepository {
+export class UserMongoRepository implements SaveUserRepository, LoadByEmailRepository, LoadByIdRepository {
   async save (addUserModel:AddUserModel):Promise<User> {
     const userCollection = MongoHelper.getCollection('users')
     const result = await userCollection.insertOne(addUserModel)
@@ -16,6 +17,14 @@ export class UserMongoRepository implements SaveUserRepository, LoadByEmailRepos
   async loadByEmail (email:string):Promise<User> {
     const userCollection = MongoHelper.getCollection('users')
     const result = await userCollection.findOne({ email })
+    const { _id, ...account } = result
+    account.id = _id
+    return account
+  }
+
+  async loadById (id:string):Promise<User> {
+    const userCollection = MongoHelper.getCollection('users')
+    const result = await userCollection.findOne({ _id: id })
     const { _id, ...account } = result
     account.id = _id
     return account
