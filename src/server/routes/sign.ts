9fdc58b2092332fe60f24571e3eb855f787e9ec7
vilmacio <vilmacio@users.noreply.github.com>
2020-10-12 22:07:19 -0300

    feat: initial profile controller

--- src/server/routes/sign.ts
+++ src/server/routes/sign.ts
@@ -9,6 +9,10 @@ export default (router: Router) => {
   router.post('/signup', expressAdapter(signupController()))
   router.post('/login', expressAdapter(loginController()))
   router.get('/profile', middlewareAdapter(authorizationMiddleware()), (req, res) => {
-    res.json(req.body)
+    const { name, email } = req.body
+    res.json({
+      name,
+      email
+    })
   })
 }
